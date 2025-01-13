be9862d3a5bf5557786df8f4911deef8439ad916
senor-huevo <137343393+senor-huevo@users.noreply.github.com>
2025-01-13 02:09:59 -0500

    Add files via upload

--- Sigils/SigilScript/Clinger.gd
+++ Sigils/SigilScript/Clinger.gd
@@ -12,19 +12,24 @@ func handle_event(event: String, params: Array):
 				var curslot = card.slot_idx()
 				var sprintSigil = card.get_node("CardBody/Sigils/Row1").get_child(card.card_data["sigils"].find(movSigil))
 				
+				# wait a bit so Clinger happens later
+				yield(fightManager.get_tree().create_timer(0.02), "timeout")
+				
 				# if the left is empty
 				if curslot < slot_idx:
 					sprintSigil.flip_h = false
-					if slot_idx <= 3 and slot_idx > 1 and slotManager.is_slot_empty(cardSlots[slot_idx - 1]):
-						offset = -1
-					elif slot_idx <= 3 and slot_idx > 1 and slotManager.is_slot_empty(cardSlots[slot_idx - 2]) and (slot_idx - 1) != curslot:
-						offset = -2
-				if curslot > slot_idx:
+					if (slot_idx - 1) != curslot:
+						if slot_idx <= 3 and slot_idx > 1 and slotManager.is_slot_empty(cardSlots[slot_idx - 1]):
+							offset = -1
+						elif slot_idx <= 3 and slot_idx > 1 and slotManager.is_slot_empty(cardSlots[slot_idx - 2]):
+							offset = -2
+				elif curslot > slot_idx:
 					sprintSigil.flip_h = true
+					if (slot_idx + 1) != curslot:
 					 # if the right is empty
-					if slot_idx < 2 and slot_idx >= 0 and slotManager.is_slot_empty(cardSlots[slot_idx + 1]):
-						offset = 1
-					elif slot_idx < 2 and slot_idx >= 0 and slotManager.is_slot_empty(cardSlots[slot_idx + 2]) and (slot_idx + 1) != curslot:
-						offset = 2
+						if slot_idx < 2 and slot_idx >= 0 and slotManager.is_slot_empty(cardSlots[slot_idx + 1]):
+							offset = 1
+						elif slot_idx < 2 and slot_idx >= 0 and slotManager.is_slot_empty(cardSlots[slot_idx + 2]):
+							offset = 2
 				if offset != 0:
 					card.move_to_parent(cardSlots[slot_idx + offset])
