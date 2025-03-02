f2568ee23cf18d30ef32a66ba05b0de7c757ddad
senor-huevo <137343393+senor-huevo@users.noreply.github.com>
2025-03-02 18:42:54 -0500

    Add files via upload

---
+++ Sigils/SigilScript/Scavenger(Goofy).gd
@@ -0,0 +1,25 @@
+extends SigilEffect
+
+# This is called whenever something happens that might trigger a sigil, with 'event' representing what happened
+func handle_event(event: String, params: Array):
+
+	if event == "card_perished" and not card.in_hand and (params[0].get_parent().get_parent().name == "PlayerSlots") != isFriendly:
+		if not params[0].has_sigil("Boneless"):
+			if isFriendly:
+				if params[0].has_sigil("Bone King"):
+					fightManager.add_bones(4)
+					if fightManager.opponent_bones >= 0:
+						fightManager.add_opponent_bones(-4)
+				else: 
+					fightManager.add_bones(1)
+				if fightManager.opponent_bones >= 0:
+					fightManager.add_opponent_bones(-1)
+			else:
+				if params[0].has_sigil("Bone King"):
+					fightManager.add_opponent_bones(4)
+					if fightManager.bones >= 0: 
+						fightManager.add_bones(-4)
+				else:
+					fightManager.add_opponent_bones(1)
+					if fightManager.bones >= 0:
+						fightManager.add_bones(-1)
